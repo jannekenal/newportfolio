5b0311b68ad650619fad9d1bc588f3a8da5e9519
Janneke Wolbers <32904213+jannekenal@users.noreply.github.com>
2018-01-17 14:08:55 +0100

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,41 @@
+
+jQuery(function () {
+	"use strict";
+    
+    /*global jQuery $*/ 
+	//Slider 
+	$(document).ready(function(){
+		
+		jQuery('.skillbar').each(function() {
+			jQuery(this).appear(function() {
+				jQuery(this).find('.count-bar').animate({
+					width:jQuery(this).attr('data-percent')
+				},3000);
+				var percent = jQuery(this).attr('data-percent');
+				jQuery(this).find('.count').html('<span>' + percent + '</span>');
+			});
+		});	
+	});
+	
+}());
+
+
+jQuery(function () {
+	"use strict";
+    
+    /*global jQuery $*/ 
+	//Slider 
+	$(document).ready(function(){
+		
+		jQuery('.goalbar').each(function() {
+			jQuery(this).appear(function() {
+				jQuery(this).find('.count-bar').animate({
+					width:jQuery(this).attr('data-percent')
+				},3000);
+				var percent = jQuery(this).attr('data-percent');
+				jQuery(this).find('.count').html('<span>' + percent + '</span>');
+			});
+		});	
+	});
+	
+}());
---
+++ dbh.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbServername="fdb17.biz.nf";
+$dbUsername="2340655_portfolio2";
+$dbPassword="Nomlexler467!!";
+$dbName="2340655_portfolio2";
+
+$conn=mysqli_connect($dbServername,$dbUsername,$dbPassword,$dbName);
